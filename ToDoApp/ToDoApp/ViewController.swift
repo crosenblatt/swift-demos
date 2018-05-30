161567b3c003c2e3d4823d070e7addfb0e92f489
crosenblatt <rosench13@gmail.com>
2018-05-30 10:53:05 -0400

    beginning of todo app

---
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Christopher Rosenblatt on 5/30/18.
+//  Copyright © 2018 crosenblatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
