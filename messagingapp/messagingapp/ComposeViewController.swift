c12ee1d1881d2940e07a70166c0454ba40931620
crosenblatt <rosench13@gmail.com>
2018-06-12 14:24:20 -0400

    messaging app ep 4

---
+++ messagingapp/messagingapp/ComposeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ComposeViewController.swift
+//  messagingapp
+//
+//  Created by Christopher Rosenblatt on 6/12/18.
+//  Copyright © 2018 crosenblatt. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addPost(_ sender: Any) {
+        //Add Post to Firebase
+        
+        //Dismiss Popover
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelPost(_ sender: Any) {
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
